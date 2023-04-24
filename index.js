3bb4a2e54aa35664ee612be966cab0853fc0529e
Necdet UYGUR <necdet.uygur@gmail.com>
2023-04-25 00:20:30 +0300

    All addresses

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 3000;
 const KullaniciRoutes = require("./routes/Kullanici");
 const StokRoutes = require("./routes/Stok");
 const StokLogRoutes = require("./routes/StokLog");
+const os = require("os");
+const networkInterfaces = os.networkInterfaces();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -24,5 +26,13 @@ app.use("/stok", StokRoutes);
 app.use("/stok-log", StokLogRoutes);
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log("___");
+  Object.keys(networkInterfaces).forEach((x) => {
+    console.log(`http://${networkInterfaces[x][1].address}:${port}`);
+  });
+  console.log("___");
+  Object.keys(networkInterfaces).forEach((x) => {
+    console.log(`https://${networkInterfaces[x][1].address}:${port}`);
+  });
+  console.log("___");
 });
